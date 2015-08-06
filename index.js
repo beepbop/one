a90a4eb91ad12d9f64fad2d57d09f30177dde3b1
Brian Woodward <brian.woodward@gmail.com>
2015-08-06 02:11:56 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * one <https://github.com/beepbop/one>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
